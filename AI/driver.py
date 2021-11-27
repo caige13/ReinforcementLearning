a9e6a4b65044a705914f831eb0d9ef3d68e9aad8
jrakov <jarodrakoff@gmail.com>
2021-11-26 23:18:35 -0600

    finished utility function

--- AI/driver.py
+++ AI/driver.py
@@ -1,6 +1,6 @@
 from alpha_beta import AlphaBeta
-from q_learning import Q_Learning_AI
-from utility_functions import play_n_games, pretty_outcome_display, plot_end_game_information
+from q_learning import QLearningAI
+from utility_functions import PlayNGames, PrintOutcome, PlotEndGameInformation
 from matplotlib.pyplot import show
 
 LEARNING_RATE = .005
@@ -14,8 +14,8 @@ ALPHA_BETA_DEPTH = 2
 TRAINING_MOVE_LIMIT = 500
 VALIDATION_MOVE_LIMIT = 1000
 TESTING_MOVE_LIMIT = 2000
-PLAYER1 = Q_Learning_AI(True, LEARNING_RATE, DISCOUNT_FACTOR,
-                        the_random_move_probability=TRAINING_RANDOM_MOVE_PROBABILITY)  # , info_location="data.json")
+PLAYER1 = QLearningAI(True, LEARNING_RATE, DISCOUNT_FACTOR,
+                      the_random_move_probability=TRAINING_RANDOM_MOVE_PROBABILITY)  # , info_location="data.json")
 PLAYER2 = AlphaBeta(False, ALPHA_BETA_DEPTH)
 # PLAYER3 = Alpha_beta(False, 1)
 PLAYER4 = AlphaBeta(False, 3)
@@ -27,26 +27,26 @@ PLAYER4 = AlphaBeta(False, 3)
 training_info = []
 validation_info = []
 for j in range(NUM_TRAINING_ROUNDS):
-    training_info.extend(play_n_games(PLAYER1, PLAYER2, NUM_GAMES_TO_TRAIN, TRAINING_MOVE_LIMIT))
-    PLAYER1.print_transition_information(PLAYER1.get_transitions_information())
-    PLAYER1.set_random_move_probability(0)
+    training_info.extend(PlayNGames(PLAYER1, PLAYER2, NUM_GAMES_TO_TRAIN, TRAINING_MOVE_LIMIT))
+    PLAYER1.print_transition_info(PLAYER1.get_transitions_info())
+    PLAYER1.set_likelihood_random_move_(0)
     PLAYER1.set_learning_rate(0)
-    validation_info.extend(play_n_games(PLAYER1, PLAYER4, NUM_VALIDATION_GAMES, VALIDATION_MOVE_LIMIT))
+    validation_info.extend(PlayNGames(PLAYER1, PLAYER4, NUM_VALIDATION_GAMES, VALIDATION_MOVE_LIMIT))
     print("Round " + str(j + 1) + " completed!")
-    PLAYER1.set_random_move_probability(TRAINING_RANDOM_MOVE_PROBABILITY)
+    PLAYER1.set_likelihood_random_move_(TRAINING_RANDOM_MOVE_PROBABILITY)
     PLAYER1.set_learning_rate(LEARNING_RATE)
     # print("")
     # PLAYER1.print_transition_information(PLAYER1.get_transitions_information())
     print("")
     PLAYER1.save_transition_information()
 
-plot_end_game_information(training_info, 200, "Training Information")
-plot_end_game_information(validation_info, NUM_VALIDATION_GAMES, "Validation Information")
+PlotEndGameInformation(training_info, 200, "Training Information")
+PlotEndGameInformation(validation_info, NUM_VALIDATION_GAMES, "Validation Information")
 show()
 
-pretty_outcome_display(training_info)
+PrintOutcome(training_info)
 print("")
-pretty_outcome_display(validation_info)
+PrintOutcome(validation_info)
 
 """
 
